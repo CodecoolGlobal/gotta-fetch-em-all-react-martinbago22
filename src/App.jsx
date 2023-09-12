fce40f40362ef685120fb03b5d9ed8bbab4acea8
Dori Balogh <132360287+DoraDru@users.noreply.github.com>
2023-09-12 15:10:21 +0200

    add areas component and reutrn with a pokemon name

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import Locations from './components/Locations'
+import Areas from './components/Areas';
 
 function App() {
   const [page, SetPage] = useState('Location');
   const [area, setArea] = useState(null);
+  const [enemyPokemon, setEnemyPokemon] = useState(null);
 
   function handleClick(locationUrl) {
     setArea(locationUrl);
-    console.log(area);
+  }
+
+  function handleEnemyPokemon(pokemon){
+    setEnemyPokemon(pokemon)
+    console.log(enemyPokemon)
   }
 
   return (
     <>
-    {area ? (<div>{area}</div>) : (<Locations onSelect={handleClick}></Locations>)}
+    {area ? (<Areas url={area} onPokemon={handleEnemyPokemon}/>) : (<Locations onSelect={handleClick}></Locations>)}
     
     </>
   )
